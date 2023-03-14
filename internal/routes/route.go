74c0ced1c6f21bae3605bd232bdf0d5afda9f69e
EndlessNigth <Pustobaev_v.a@outlook.com>
2023-03-14 21:35:16 +0300

    delete .env

--- internal/routes/route.go
+++ internal/routes/route.go
@@ -1,12 +1,18 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/spf13/viper"
 )
 
 func InitRoutes() {
 	e := echo.New()
+
+	e.GET("/ping", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "ok")
+	})
 
 	antiBruteForce := e.Group("/api/antiBruteForce")
 
